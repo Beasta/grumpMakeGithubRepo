78246f5a189448164d8ef8a1e6cf00dbeb50e97f
Beasta <barryblaha@gmail.com>
2017-05-18 17:02:38 -0700

    Fix issue

--- makeGithubRepo.sh
+++ makeGithubRepo.sh
@@ -3,5 +3,10 @@ echo "what is your github username?"
 read githubUsername
 echo "what should your repo be named?"
 read repoName
+theName='{"name":"'
+theName+=${repoName}
+theName+='"}'
+theCurl="-u ${githubUsername} https://api.github.com/user/repos -d"
+theCurl+=theName
 curl -u ${githubUsername} https://api.github.com/user/repos -d '{"name":"${repoName}"}'
 
