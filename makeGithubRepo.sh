085f6073ac1365cf7ca1d21df2a393775b98830c
Beasta <barryblaha@gmail.com>
2017-05-23 00:58:03 -0700

    add push option

--- makeGithubRepo.sh
+++ makeGithubRepo.sh
@@ -9,3 +9,11 @@ theName+='"}'
 theCurl="-u ${githubUsername} https://api.github.com/user/repos -d "
 theCurl+=$theName
 curl $theCurl
+echo "add remote and push now (y/n)?"
+read push
+if [ ${push} -eq 'y' ]
+  then
+    origin="https://github.com/${githubUsername}/${repoName}"
+    git remote add origin ${origin}
+    git push --set-upstream origin master
+fi
